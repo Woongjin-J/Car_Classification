9bd970f6410dc2da7e52e367f7afb9acf2586d7c
Woongjin-J <67777378+Woongjin-J@users.noreply.github.com>
2021-09-29 20:58:39 +0900

    finalized
    finished styling

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from __future__ import print_function
 import os
-import requests
 import json
+import requests
 from PIL import Image
 from flask import Flask, render_template, url_for, request, redirect, \
   send_from_directory, jsonify
@@ -15,18 +14,13 @@ UPLOAD_FOLDER = 'uploads'
 app.config['VALID_EXTENSIONS'] = ['jpg', 'jpeg', 'png']
 app.config['UPLOAD_PATH'] = UPLOAD_FOLDER
 
-# home page with two button (camera & local file)
-@app.route('/')
-def home():
-  return render_template('home.html')
-
 # local file page where one can upload local image
-@app.route('/local_file')
-def local_file():
-  return render_template('local_file.html')
+@app.route('/')
+def classification():
+  return render_template('classification.html')
 
 # Make prediction of the image uploaded
-@app.route('/local_file', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_image():
    if request.method == 'POST':
       # Receives POST request and save the uploaded image to the FLask's uploads folder
@@ -39,32 +33,12 @@ def upload_image():
       response = requests.post(prediction_url, data=open(img_path, "rb"), headers=headers)
       response.raise_for_status()
       prediction = response.json()["predictions"][0]
-      return render_template('local_file.html', prediction=prediction, img_path=img_path)
+      return render_template("classification.html", prediction=prediction, img_path=img_path)
 
 # Save the image to /uploads folder
 @app.route('/uploads/<filename>')
 def upload(filename):
   return send_from_directory(app.config['UPLOAD_PATH'], filename)
-
-# Camera page
-@app.route('/camera')
-def camera():
-  return render_template('/camera.html')
-
-@app.route('/camera', methods=['POST'])
-def upload_camera():
-   if request.method == 'POST':
-      # Receives POST request and save the uploaded image to the FLask's uploads folder
-      img_file = request.files['file']
-      filename = secure_filename(img_file.filename)
-      img_path = os.path.join(app.config['UPLOAD_PATH'], filename)
-      img_file.save(img_path)
-      print("it's working")
-      # Sends the image in a format of byte array to the API for predictions saved in json
-      response = requests.post(prediction_url, data=open(img_path, "rb"), headers=headers)
-      response.raise_for_status()
-      prediction = response.json()["predictions"][0]
-      return render_template('camera.html', prediction=prediction, img_path=img_path)
 
 if __name__ == '__main__':
   app.run(debug=True)
